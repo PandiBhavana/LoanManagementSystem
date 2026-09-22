70862ae25f08c6f95dbbb0c492f8cdeea24a3c6f
Bhavana Pandi <bhavana262003@gmail.com>
2026-09-22 23:22:03 +0530

    Add Dao Implementations And Service Layer Crud Operations

---
+++ src/main/java/com/loanmanagement/service/impl/LoanLoanApplicationServiceImpl.java
@@ -0,0 +1,30 @@
+package com.loanmanagement.service.impl;
+
+import com.loanmanagement.dao.LoanApplicationDao;
+import com.loanmanagement.dao.impl.LoanApplicationDaoImpl;
+import com.loanmanagement.model.LoanApplication;
+import com.loanmanagement.service.LoanApplicationService;
+
+public class LoanLoanApplicationServiceImpl implements LoanApplicationService {
+     LoanApplicationDao loanApplicationDao = new LoanApplicationDaoImpl();
+    @Override
+    public void addApplication(LoanApplication application) {
+        loanApplicationDao.addLoanApplication(application);
+    }
+
+    @Override
+    public LoanApplication getApplicationById(int applicationId) {
+        return loanApplicationDao.getLoanApplicationById(applicationId);
+
+    }
+
+    @Override
+    public void updateApplication(LoanApplication application) {
+        loanApplicationDao.updateLoanApplication(application);
+    }
+
+    @Override
+    public void deleteApplication(int applicationId) {
+        loanApplicationDao.deleteLoanApplication(applicationId);
+    }
+}
