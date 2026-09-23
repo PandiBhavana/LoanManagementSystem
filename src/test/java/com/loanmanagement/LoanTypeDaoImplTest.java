a3e2d969d8a0aaa4f4144ecbcbc348ca9c6b76dd
Bhavana Pandi <bhavana262003@gmail.com>
2026-09-23 20:53:55 +0530

    Added JUnit tests

--- src/test/java/com/loanmanagement/LoanTypeDaoImplTest.java
+++ src/test/java/com/loanmanagement/LoanTypeDaoImplTest.java
@@ -1,4 +1,50 @@
 package com.loanmanagement;
 
+import com.loanmanagement.dao.LoanTypeDao;
+import com.loanmanagement.dao.impl.LoanTypeDaoImpl;
+import com.loanmanagement.model.LoanType;
+import org.junit.jupiter.api.Test;
+
 public class LoanTypeDaoImplTest {
+     LoanTypeDao loanTypeDao = new LoanTypeDaoImpl();
+    @Test
+    public void testAddLoanType() {
+
+        LoanType loanType = new LoanType();
+
+        loanType.setName("Education Loan");
+        loanType.setDescription("Loan for education");
+        loanType.setInterestRate(8.5);
+        loanType.setMinAmount(50000);
+        loanType.setMaxAmount(500000);
+        loanType.setMaxTenureMonths(60);
+        loanType.setStatus("ACTIVE");
+
+        loanTypeDao.addLoanType(loanType);
+    }
+    @Test
+    public void testGetLoanTypeById() {
+
+        LoanType loanType = loanTypeDao.getLoanTypeById(5);
+
+        System.out.println("Loan Type ID: " + loanType.getLoanTypeId());
+        System.out.println("Name: " + loanType.getName());
+        System.out.println("Interest Rate: " + loanType.getInterestRate());
+    }
+    @Test
+    public void testUpdateLoanType() {
+
+        LoanType loanType = loanTypeDao.getLoanTypeById(5);
+
+        loanType.setInterestRate(9.5);
+        loanType.setMaxAmount(700000);
+
+        loanTypeDao.updateLoanType(loanType);
+    }
+    @Test
+    public void testDeleteLoanType() {
+
+        loanTypeDao.deleteLoanType(5);
+    }
+
 }
--- src/test/java/com/loanmanagement/UserDaoImplTest.java
+++ src/test/java/com/loanmanagement/UserDaoImplTest.java
@@ -1,4 +1,46 @@
 package com.loanmanagement;
 
+import com.loanmanagement.dao.UserDao;
+import com.loanmanagement.dao.impl.UserDaoImpl;
+import com.loanmanagement.model.User;
+import org.junit.jupiter.api.Test;
+
 public class UserDaoImplTest {
+   private UserDao userDao = new UserDaoImpl();
+    @Test
+    public void testAddUser() {
+
+        User user = new User();
+
+        user.setUsername("junituser");
+        user.setPassword("12345");
+        user.setRole("CUSTOMER");
+        user.setStatus("ACTIVE");
+
+        userDao.addUser(user);
+    }
+    @Test
+    public void testGetUserById() {
+
+        User user = userDao.getUserById(17);
+
+        System.out.println("User ID: " + user.getUserId());
+        System.out.println("Username: " + user.getUsername());
+        System.out.println("Role: " + user.getRole());
+    }
+    @Test
+    public void testUpdateUser() {
+
+        User user = userDao.getUserById(17);
+
+        user.setPassword("newpassword");
+        user.setStatus("ACTIVE");
+
+        userDao.updateUser(user);
+    }
+    @Test
+    public void testDeleteUser() {
+
+        userDao.deleteUser(17);
+    }
 }
